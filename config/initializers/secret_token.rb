3dd5f9ec39314351eb3cbbff55a0a4bbaeec784c
Andrew Kharzin <andrewkharzin@MacBook-Pro-Andrew-Kharzin.local>
2013-05-01 02:15:38 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myunite::Application.config.secret_token = '47b451878f1d6ed4bc71fb8eff8c2d8c7c35d3f3f1d9ff0f0723fd307b76da5b3779b54d1946d1a4861fff75f5675fe114f1132921832796477fd30a2c7a891e'

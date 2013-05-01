f53eb4362bedcfe0a5cfa261ee0490749177fbba
Andrew Kharzin <andrewkharzin@MacBook-Pro-Andrew-Kharzin.local>
2013-05-01 14:45:02 +0400

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Myunite::Application.routes.draw do
     resources :users
+    resources :sessions, only: [:new, :create, :destroy]
+    
     root to: 'static_pages#home'
     
     match '/signup',  to: 'users#new'
@@ -7,6 +9,8 @@ Myunite::Application.routes.draw do
     match '/help',    to: 'static_pages#help'
     match '/about',   to: 'static_pages#about'
     match '/contact', to: 'static_pages#contact'
+    match '/signin',  to: 'sessions#new'
+    match '/signout', to: 'sessions#destroy', via: :delete
     
 end
   

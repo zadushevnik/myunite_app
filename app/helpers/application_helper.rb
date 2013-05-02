d9afe2c1eadd84f7a39861f2d58644af895d6480
Andrew Kharzin <andrewkharzin@MacBook-Pro-Andrew-Kharzin.local>
2013-05-02 11:27:38 +0400

    Add user following

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,20 @@ module ApplicationHelper
         "#{base_title} | #{page_title}"
       end
     end
+    
+    # Flash messaging alert etc.
+    def bootstrap_class_for flash_type
+        case flash_type
+          when :success
+            "alert-success"
+          when :error
+            "error"
+          when :alert
+            "alert-block"
+          when :notice
+            "alert-info"
+          else
+            flash_type.to_s
+        end
+    end
 end
